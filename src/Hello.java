db25fc37217418689f3ceed26f590182f526e012
Tom <thomas.spencer@codurance.com>
2021-06-08 07:33:09 +0100

    Edit

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello " + args[0]);
 
-    int myFirstNumber = (10 + 5) + (2 * 3);
+    int myFirstNumber = (10 + 5) + (2 * 10);
     System.out.println("My number: " + myFirstNumber);
   }
 }
