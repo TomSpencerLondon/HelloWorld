beeeb3bb2d7c10a07d4c7be79ffe4641994e9aa3
Tom <thomas.spencer@codurance.com>
2021-06-08 07:35:11 +0100

    Add int

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     System.out.println("Hello " + args[0]);
 
     int myFirstNumber = (10 + 5) + (2 * 10);
+    int mySecondNumber = 12;
+    int myThirdNumber = 6;
     System.out.println("My number: " + myFirstNumber);
   }
 }
