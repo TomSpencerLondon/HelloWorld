2c541bea3b463af4cbe5abd8919f4c481bb23e76
Tom <thomas.spencer@codurance.com>
2021-06-08 07:32:15 +0100

    Add operators

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello " + args[0]);
 
-    int myFirstNumber = 5;
+    int myFirstNumber = (10 + 5) + (2 * 3);
     System.out.println("My number: " + myFirstNumber);
   }
 }
