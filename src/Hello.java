8f2a568350ce5b9bd6003e89054dbe59be3e866c
Tom <thomas.spencer@codurance.com>
2021-06-08 07:28:39 +0100

    Add print for main

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
   public static void main(String[] args) {
     System.out.println("Hello " + args[0]);
+
+    int myFirstNumber = 5;
+    System.out.println("My number: " + myFirstNumber);
   }
 }
